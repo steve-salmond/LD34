004f02cda6f0ccf2a3c22d12a82f7a7f1c473c6e
Steve Salmond <steve.salmond@smallworlds.com>
2015-12-13 10:53:50 +1300

    Starting on a screen flash effect.

---
+++ Assets/Scripts/UI/UIScreenFlash.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using DG.Tweening;
+
+public class UIScreenFlash : Singleton<UIScreenFlash>
+{
+
+    public Graphic Graphic
+    { get; private set; }
+
+    private void Awake()
+    {
+        Graphic = GetComponent<Graphic>();
+    }
+
+    public void Flash(Color c, float duration = 0.25f)
+    {
+        Graphic.DOBlendableColor(c, duration);
+
+        // TODO: Revert to transparent.
+    }
+    
+	
+}
